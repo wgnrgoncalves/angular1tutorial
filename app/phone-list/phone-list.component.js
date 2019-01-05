254d66cfec52fda2855e6a80039b5bb99c48599b
Wagner Gonçalves <wgnrgoncalves@gmail.com>
2019-01-05 19:13:00 -0200

    Passo 00005 organização das pastas

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -1,13 +1,9 @@
+'use strict';
+
 angular.
-    module('phonecatApp').
+    module('phoneList').
     component('phoneList', {
-        template:
-            '<ul>' +
-                '<li ng-repeat="phone in $ctrl.phones">' +
-                    '<span>{{phone.name}}</span>' +
-                    '<p>{{phone.snippet}}</p>' +
-                '</li>' +
-            '</ul>',
+        templateUrl: 'phone-list/phone-list.template.html',
         controller: function PhoneListController() {
             this.phones = [
                 {
