a4c5e01e94f2bdad79ec7ca03b350b0dd9ac83fd
Wagner Gonçalves <wgnrgoncalves@gmail.com>
2019-01-05 15:28:50 -0200

    Components passo 00004

--- app/app.js
+++ app/app.js
@@ -1,20 +1,2 @@
-var phonecatApp = angular.module('phonecatApp', []);
-phonecatApp.controller('PhoneListController',
-    function PhoneListController($scope) {
-        $scope.phones = [
-            {
-                name: "Nexus S",
-                snippet: 'Fast  just got faster with Nexsus S.'
-            },
-            {
-                name: 'Motorola XOOM™ with Wi-Fi',
-                snippet: 'The Next, Next Generation tablet.'
-            }, {
-                name: 'MOTOROLA XOOM™',
-                snippet: 'The Next, Next Generation tablet.'
-            }
-        ];
-        $scope.name = 'world';
-
-    }
-);
+'use strict';
+angular.module('phonecatApp',[]);
