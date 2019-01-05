68d3a237fd0b0d7db4d4107504c3282e438e4090
Wagner Gonçalves <wgnrgoncalves@gmail.com>
2019-01-05 14:47:41 -0200

    Fiz um teste criando outro controller

--- app/app.js
+++ app/app.js
@@ -1,20 +1,41 @@
 var phonecatApp = angular.module('phonecatApp', []);
-phonecatApp.controller('PhoneListController',
-    function PhoneListController($scope) {
-        $scope.phones = [
+phonecatApp
+    .controller('PhoneListController',
+        function PhoneListController($scope) {
+            $scope.phones = [
+                {
+                    name: "Nexus S",
+                    snippet: 'Fast  just got faster with Nexsus S.'
+                },
+                {
+                    name: 'Motorola XOOM™ with Wi-Fi',
+                    snippet: 'The Next, Next Generation tablet.'
+                }, {
+                    name: 'MOTOROLA XOOM™',
+                    snippet: 'The Next, Next Generation tablet.'
+                }
+            ];
+            $scope.name = 'world';
+
+        }
+    ).controller('OutraLista',
+    function OutraLista($scope){
+        $scope.carros = [
             {
-                name: "Nexus S",
-                snippet: 'Fast  just got faster with Nexsus S.'
+                nome : 'Celta',
+                marca : 'GM'
             },
             {
-                name: 'Motorola XOOM™ with Wi-Fi',
-                snippet: 'The Next, Next Generation tablet.'
-            }, {
-                name: 'MOTOROLA XOOM™',
-                snippet: 'The Next, Next Generation tablet.'
+                nome : 'Fusca',
+                marca : 'VW'
+            },
+            {
+                nome : 'Civic',
+                marca : 'Honda'
+            },
+            {
+                nome : 'Uno',
+                marca : 'Fiat'
             }
-        ];
-        $scope.name = 'world';
-
-    }
-);
+        ]
+    });
