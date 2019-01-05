b01a66fe4c778899150548fcb11f8be983d2d792
Wagner Gonçalves <wgnrgoncalves@gmail.com>
2019-01-05 14:40:46 -0200

    Passo 3 https://docs.angularjs.org/tutorial/step_02

---
+++ app/app.js
@@ -0,0 +1,20 @@
+var phonecatApp = angular.module('phonecatApp', []);
+phonecatApp.controller('PhoneListController',
+    function PhoneListController($scope) {
+        $scope.phones = [
+            {
+                name: "Nexus S",
+                snippet: 'Fast  just got faster with Nexsus S.'
+            },
+            {
+                name: 'Motorola XOOM™ with Wi-Fi',
+                snippet: 'The Next, Next Generation tablet.'
+            }, {
+                name: 'MOTOROLA XOOM™',
+                snippet: 'The Next, Next Generation tablet.'
+            }
+        ];
+        $scope.name = 'world';
+
+    }
+);
