7eef811a79cfa6a51a164311ba07fc0fde90c518
Wagner Gonçalves <wgnrgoncalves@gmail.com>
2019-01-05 22:55:17 -0200

    Eventos

--- app/phone-detail/phone-detail.component.js
+++ app/phone-detail/phone-detail.component.js
@@ -7,10 +7,18 @@ angular.
         controller: ['$http', '$routeParams',
             function PhoneDetailController($http, $routeParams){
                 var self = this;
+                self.setImage = function setImage(imageUrl){
+                    self.mainImageUrl = imageUrl;
+                }
+
+                self.onDbclick = function onDbClick(imageUrl){
+                    alert('You double-clicked image: ' + imageUrl);
+                }
                 $http.
                     get('phones/' + $routeParams.phoneId + '.json').
                     then(function(response){
                         self.phone = response.data;
+                        self.setImage(self.phone.images[0]);
                     });
             }
         ]
